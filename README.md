02a6e6e69e162e0f6aa130f7f032c1d88b4c6be8
Sneh Kr <102907785+snehkr@users.noreply.github.com>
2022-05-18 08:55:34 +0530

    PC CLEANER v1.0

---
+++ README.md
@@ -0,0 +1,13 @@
+<img src="https://img.shields.io/github/watchers/snehkr/PC-Cleaner?color=007bdb&label=Watchers" alt="shield.png"></a>
+<img src="https://img.shields.io/github/stars/snehkr/PC-Cleaner?color=007bdb&label=Stars" alt="shield.png"></a>
+
+#### PC Cleaner made by
+SNEH ❌
+KR ✅
+
+### ❔・About
+**Run as administrator if you want it to clean most of the temp files and such**
+
+**You can run it without admin but it won't have permission to delete some temp files**
+
+
---
+++ cleaner.bat
@@ -0,0 +1,24 @@
+@echo off
+echo Welcome to PC Cleaner
+echo MADE BY : SNEH KR
+echo Cleaning temporary files
+color 0C
+del /s /f /q %SYSTEMDRIVE%\windows\temp\*.*
+rd /s /q %SYSTEMDRIVE%\windows\temp
+md c:\windows\temp
+del /s /f /q %SYSTEMDRIVE%\WINDOWS\Prefetch
+del /s /f /q %temp%\*.*
+rd /s /q %temp%
+echo Done cleaning temporary files
+echo Cleaning logs
+md %temp%
+del /q /f /s %SYSTEMDRIVE%\Temp\*.*
+del /q /f /s %WINDIR%\Prefetch\*.*
+del /q /f /s %SYSTEMDRIVE%\*.log
+del /q /f /s %SYSTEMDRIVE%\*.bak
+del /q /f /s %SYSTEMDRIVE%\*.gid
+echo Cleaned logs
+echo Thanks for using PC Cleaner
+echo MADE BY : SNEH KR
+pause
+cls
